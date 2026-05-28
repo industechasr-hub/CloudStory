fbfe3bf4603f01f3c4a1a3360e27feb3c957de6c
Akash <industechasr@gmail.com>
2026-05-28 12:06:48 +0530

    add middleware - json() , urlencoded, morgan

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import "dotenv/config"
+import morgan from 'morgan'
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
+
+//middleware
+app.use(express.json)
+
+app.use(express.urlencoded)
+
+app.use(morgan('dev'))
 
 app.get("/", (req, res) => {
     res.send("hello from Cloud Story Server")
@@ -11,6 +19,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running at https://localhost:${PORT}`);
-
 
 })
