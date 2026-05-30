e59e0489f36274f48ff883f79dddefca9b24de0b
Akash <industechasr@gmail.com>
2026-05-30 08:29:53 +0530

    loader added

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar'
+import Loader from '../../Components/loader/Loader'
 
 function Home() {
   return (
     <>
     <Navbar/>
     <h1>Home</h1>
+    {/* <Loader/> */}
+   
     </>
     
   )
