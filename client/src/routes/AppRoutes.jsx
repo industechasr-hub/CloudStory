b4cd5f62ae4ca5334c4e10e3c61ee61acf027c69
Akash <industechasr@gmail.com>
2026-06-21 12:10:25 +0530

    bug fixes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,14 +1,14 @@
 
 import { Routes, Route } from 'react-router-dom'
 
-import Welcome from '../pages/Welcome/Welcome.jsx'
-import Login from '../pages/Login/Login.jsx'
-import Register from '../pages/Register/Register.jsx'
-import Home from '../pages/Home/Home.jsx'
-import About from '../pages/About/About.jsx'
-import Contact from '../pages/Contact/Contact.jsx'
+import Welcome from '../Pages/Welcome/Welcome.jsx'
+import Login from '../Pages/Login/Login.jsx'
+import Register from '../Pages/Register/Register.jsx'
+import Home from '../Pages/Home/Home.jsx'
+import About from '../Pages/About/About.jsx'
+import Contact from '../Pages/Contact/Contact.jsx'
 import Books from '../Pages/Books/Books.jsx'
-import Profile from '../pages/Profile/Profile.jsx'
+import Profile from '../Pages/Profile/Profile.jsx'
 import UploadBookForm from '../Pages/BookCreate/UploadBookForm.jsx'
 
 
